d2ceefac1a157456539b8fcaaa8738a1ebee187f
Bernard Thompson <bethomps@redhat.com>
2019-11-07 16:25:54 +0000

    Create basis for v2

--- app/src/main/java/com/example/donation/models/DonationStore.kt
+++
@@ -1,7 +0,0 @@
-package com.example.donation.models
-
-interface DonationStore {
-    fun findAll() : List<DonationModel>
-    fun findById(id: Long) : DonationModel?
-    fun create(donation: DonationModel)
-}
